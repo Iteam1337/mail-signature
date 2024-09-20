223eb938a54ea5f31b793e3c739a589386d762b8
Christian Landgren <christian@landgren.nu>
2024-09-20 15:53:25 +0200

    refactor: simplify server.ts by converting to ES module syntax and removing unnecessary line breaks

--- server.ts
+++ server.ts
@@ -1,27 +1,21 @@
-const express = require('express');
-const path = require('path');
+import express from 'express'
+import path from 'path'
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
 // Set Pug as the template engine
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'static')));
-
-// API endpoint to get user info
-app.get('/api/user-info', (req, res) => {
-	const email = req.headers['x-forwarded-email'] || '';
-	res.json({ email });
-});
+app.use(express.static(path.join(__dirname, 'static')))
 
 // Render the main page
 app.get('/', (req, res) => {
-	res.render('index', { email: req.headers['x-forwarded-email'] || '' });
-});
+  res.render('index', { email: req.headers['x-forwarded-email'] || '' })
+})
 
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
